2c95a5867608352131fcd8afa168210baa253035
AdarshAchu2692 <adarshachu2692@gmail.com>
2026-02-09 13:09:09 +0530

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,8 @@
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+
+export default defineConfig({
+  plugins: [react()],
+  base: "/your-website-creator/", // 👈 VERY IMPORTANT
+})
 /// <reference types="vite/client" />
